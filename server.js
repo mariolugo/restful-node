21d34707377e1cd0f90f001e0a1af39cacca4836
Mario Lugo <mariolugo23@gmail.com>
2015-07-14 16:23:59 -0700

    Created POST route

--- server.js
+++ server.js
@@ -40,6 +40,37 @@ app.get('/',function(req,res){
 // get an instance of the express router
 var apiRouter = express.Router();
 
+// middleware to use for all requests
+apiRouter.use(function(req, res, next){
+	// do logging
+	console.log('Somebody just came to our app!');
+	next();
+});
+
+// on routes that end in /users
+// ----------------------------------------------------
+apiRouter.route('/users')
+  // create a user (accessed at POST http://localhost:8080/api/users)
+  .post(function(req, res){
+    // create a new instance of the User model
+    var user = new User();
+    // set the users information (comes from the request)
+    user.name = req.body.name;
+    user.username = req.body.username;
+    user.password = req.body.password;
+    // save the user and check for errors
+    user.save(function(err){
+    	if (err){
+    		// duplicate entry
+    		if(err.code == 11000)
+    			return res.json({success: false, message: 'A user with that username already exists.'});
+    		else
+    			return res.send(err);
+    	}
+      res.json({message: 'User created!'});
+    });
+  })
+
 // test route to make sure everything is working
 // accessed at GET http://localhost:8080/api
 apiRouter.get('/', function(req,res){
